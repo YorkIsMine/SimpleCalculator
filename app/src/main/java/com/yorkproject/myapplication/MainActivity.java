0d8dde96cd5482ef6c75e134c2684e11016182d7
YorkIsMine <vladal56hack@yandex.ru>
2019-04-21 17:54:37 +0300

    Improved logic A bit of new design

--- app/src/main/java/com/yorkproject/myapplication/MainActivity.java
+++ app/src/main/java/com/yorkproject/myapplication/MainActivity.java
@@ -4,9 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    TextView resultField;
+    EditText numberField;
+    TextView operationField;
+    Integer operand = null;
+    String lastOperation = "=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,22 +23,88 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("Calculator");
+
+        resultField =(TextView) findViewById(R.id.resultField);
+        numberField = (EditText) findViewById(R.id.numberField);
+        operationField = (TextView) findViewById(R.id.operationField);
     }
-    public void onClick(View view){
-        EditText editText = findViewById(R.id.editText);
-        switch (view.getId()){
-            case R.id.four:
-                editText.setText("4");
-                break;
-            case R.id.five:
-                editText.setText("5");
-                break;
-            case R.id.plus:
-                editText.setText("+");
-                break;
-            case R.id.delete:
-                editText.setText("");
-                break;
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        outState.putString("OPERATION",lastOperation);
+        if(operand != null){
+            outState.putDouble("OPERAND",operand);
         }
+        super.onSaveInstanceState(outState);
+
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        lastOperation = savedInstanceState.getString("OPERATION");
+        operand = savedInstanceState.getInt("OPERAND");
+        resultField.setText(operand.toString());
+        operationField.setText(lastOperation);
+    }
+
+    public void onNumberClick(View view){
+        Button button = (Button) view;
+        numberField.append(button.getText());
+
+        if(lastOperation.equals("=") && operand!=null){
+            operand = null;
+        }
+    }
+
+    public void onOperationClick(View view){
+        Button button = (Button)view;
+        String op = button.getText().toString();
+        String number = numberField.getText().toString();
+
+        if(number.length()>0){
+            number = number.replace(',', '.');
+            try{
+                performOperation(Integer.valueOf(number), op);
+            }catch (NumberFormatException ex){
+                numberField.setText("");
+            }
+        }
+        lastOperation = op;
+        operationField.setText(lastOperation);
+    }
+    private void performOperation(Integer number, String operation){
+
+        if(operand ==null){
+            operand = number;
+        }
+        else{
+            if(lastOperation.equals("=")){
+                lastOperation = operation;
+            }
+            switch(lastOperation){
+                case "=":
+                    operand =number;
+                    break;
+                case "/":
+                    if(number==0){
+                        operand =0;
+                    }
+                    else{
+                        operand /=number;
+                    }
+                    break;
+                case "X":
+                    operand *=number;
+                    break;
+                case "+":
+                    operand +=number;
+                    break;
+                case "-":
+                    operand -=number;
+                    break;
+
+            }
+        }
+        resultField.setText(operand.toString().replace('.', ','));
+        numberField.setText("");
     }
 }
