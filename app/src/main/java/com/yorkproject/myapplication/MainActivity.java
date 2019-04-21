edddeaddcc94b8a9f7370304ee4925b4d2612546
YorkIsMine <vladal56hack@yandex.ru>
2019-04-21 11:33:45 +0300

    first commit

---
+++ app/src/main/java/com/yorkproject/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.yorkproject.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Calculator");
+    }
+    public void onClick(View view){
+        EditText editText = findViewById(R.id.editText);
+        Button four;
+        four = (Button) view;
+        String text = four.getText().toString();
+        editText.setText(text);
+
+    }
+}
